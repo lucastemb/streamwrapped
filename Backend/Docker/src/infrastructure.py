35b2f7408e14536c38193a15dab9fd54fcb6467e
Lucas Tembras <51347268+lucastemb@users.noreply.github.com>
2024-10-22 11:05:20 -0400

    feat: retrieve achievements and filter by not completed

--- Backend/Docker/src/infrastructure.py
+++ Backend/Docker/src/infrastructure.py
@@ -14,5 +14,20 @@ app = Flask(__name__)
 def get_user(user_id):
 	return jsonify(steam.users.search_user(user_id)), 200
 
+@app.route("/get-games/<user_id>")
+def get_games(user_id):
+      return jsonify(steam.users.get_owned_games(user_id)), 200
+
+@app.route("/get-achievements/<user_id>/<app_id>")
+def get_game_achievements(user_id, app_id):
+    res = steam.apps.get_user_achievements(user_id, app_id)
+    unachieved = []
+    for achievement in res["playerstats"]["achievements"]:
+        if achievement["achieved"] == 0: 
+                unachieved.append(achievement)
+    return jsonify(unachieved), 200
+    
+            
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(port=8000, debug=True)
