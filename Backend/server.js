f7103807633b6d46076246da0c63a0126b4ac1a1
Lucas Tembras <51347268+lucastemb@users.noreply.github.com>
2024-10-23 20:50:07 -0400

    feat: connected frontend to backend + personalizable achievement list

--- Backend/server.js
+++ Backend/server.js
@@ -1,12 +1,16 @@
 const express = require("express")
 const axios = require('axios')
+const cors = require('cors')
 
 const app = express()
 const PORT = 8080
 
-app.get("/get-data", async (req, res)=> {
+app.use(cors())
+
+app.get("/get-data/:playerId/:gameId", async (req, res)=> {
+    const {playerId, gameId} = req.params
     try {
-        const response = await axios.get('http://127.0.0.1:8000/get-achievements/76561198109081792/413150')
+        const response = await axios.get(`http://127.0.0.1:8000/get-achievements/${playerId}/${gameId}`)
         res.json(response.data)
     } catch(error){
         console.error('Error pinging Flask endpoint:', error.message);
