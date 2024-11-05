1c16f53619b189bc0a399e9f100c2f5815f72a94
josecast1 <jc88377@gmail.com>
2024-11-05 01:27:11 -0500

    added styling

--- frontend/client/pages/index.tsx
+++ frontend/client/pages/index.tsx
@@ -46,39 +46,44 @@ export default function Home() {
 
   return (
   <>
-   <div>
+   <div className = "bg-slate-700 h-full text-white">
     <div className="row"> 
       <div className = "flex flex-row">
       <div>
-        <label>Input One:</label>
+        <label>User ID: </label>
         <input
           type="text"
           value={playerId}
           onChange={handlePlayerChange}
+          className="rounded px-2 py-1 my-2 text-black"
         />
       </div>
 
       <div>
-        <label>Input Two:</label>
+        <label>Game ID: </label>
         <input
           type="text"
           value={gameId}
           onChange={handleGameChange}
+          className="rounded px-2 py-1 my-2 text-black"
         />
       </div>
-      <button onClick={submitToBackEnd}> Submit </button> 
+      <button 
+        onClick={submitToBackEnd}
+        className="bg-blue-500 text-white rounded px-4 py-2 my-1 ml-2"
+        > 
+        Submit </button> 
       </div>
 
       <div>
         <p>Input One: {playerId}</p>
         <p>Input Two: {gameId}</p>
         <p> Response: {Array.isArray(responseMessage) && responseMessage.filter(achievement => achievement.achieved === 0).map((achievement: any, index: number)=> (
-          <div key={index}>
-          <p>Name: {achievement.name}</p>
+          <div key={index} className="bg-slate-900 p-4 rounded-lg shadow-md my-2 mx-56">
+          <p className="font-bold">{achievement.name}</p>
           <p>Description: {achievement.description}</p>
           <p>Achieved: {achievement.achieved ? "Yes" : "No"}</p>
           <p>Unlock Time: {achievement.unlocktime}</p>
-          <hr />
       </div>
         ))} </p>
       </div>
