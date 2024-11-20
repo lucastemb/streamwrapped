ec8807231fe9c729e4cbe6c9f6e35ff4002b62da
Lucas Tembras <51347268+lucastemb@users.noreply.github.com>
2024-11-20 17:21:25 -0500

    feat: added more authentication features

--- Backend/server.js
+++ Backend/server.js
@@ -1,9 +1,35 @@
 const express = require("express")
 const axios = require('axios')
 const cors = require('cors')
+require('dotenv').config();
 
 const app = express()
 const PORT = 8080
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = `mongodb+srv://steamwrapped_admin:${process.env.MONGO_PWD}@steamwrapped.re39x.mongodb.net/?retryWrites=true&w=majority&appName=steamwrapped`;
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function connectToDatabase() {
+    try {
+      await client.connect();
+      console.log("Connected to MongoDB");
+    } catch (error) {
+      console.error("Failed to connect to MongoDB:", error.message);
+      process.exit(1); // Exit if connection fails
+    }
+  }
+  
+  // Call this at app startup
+connectToDatabase();
+
 
 app.use(cors())
 //TODO: Consider condensing below functions using a helper function as follows:
@@ -47,14 +73,37 @@ app.get("/get-games/:playerId", async (req, res) => {
 
 //login attempt
 //NOTE: this is a PLACEHOLDER
-app.get("/attempt-login/:user_login", async (req, res) => {
-    const { user_login } = req.params;
+app.get("/exists-user/:email", async (req, res) => {
+        const { email } = req.params
+        try {
+          //get db 
+          const database = client.db("steamwrapped");
+          //users
+          const users = database.collection("users");
+          const query = { email: email };
+          const user = await users.findOne(query);
+          if (!user) {
+           return res.json({exists: false, message: "Email not found"});
+          }
+          return res.json({exists:true, user});
+        } catch(error) {
+            return res.json({error: "Internal server error"});
+        }
+});
+
+app.post("/add-user/:email/:steamID/:steamURL", async (req, res) => {
+    const { email, steamID, steamURL } = req.params;
     try {
-        const response = await axios.get(`http://127.0.0.1:8000/attempt-login/${user_login}`);
-        res.json(response.data);
-    } catch (error) {
-        console.error('Error pinging Flask endpoint:', error.message);
-        res.status(500).send('Error pinging Flask server');
+        //get db 
+        const database = client.db("steamwrapped");
+        //users
+        const users = database.collection("users");
+        const document = { email: email, steamID: steamID, steamURL: steamURL };
+        const result = await users.insertOne(document);
+        return res.status(201).json({ message: "User added successfully", user: document });
+    }
+    catch (error) {
+        return res.status(500).json({error: "Internal server error"});
     }
 });
 
--- frontend/client/pages/index.tsx
+++ frontend/client/pages/index.tsx
@@ -3,15 +3,31 @@ import Dashboard from "./dashboard";
 import { GoogleOAuthProvider } from "@react-oauth/google";
 import { GoogleLogin } from '@react-oauth/google';
 import {jwtDecode} from "jwt-decode"
+import axios from "axios"; 
 
 export default function Home() {
   const [loggedIn, setLoggedIn] = useState<boolean>(false);
   const [failed, setFailed] = useState<boolean | undefined>(undefined);
   const [email, setEmail] = useState<string>("");
+  const [exists, setExists] = useState<boolean>();
 
   useEffect(()=> {
-    console.log(email);
-  },[email])
+    if(loggedIn && email){
+      console.log(email)
+      axios.get(`http://localhost:8080/exists-user/${email}`).then((response)=> {
+        if(response.data.exists === true){
+          setExists(true)
+        }
+        else if (response.data.exists === false) {
+          setExists(false)
+        }
+      }).catch((error) => {
+        console.error('Error', error)
+        setExists(false)
+      })
+    }
+  },[loggedIn, email])
+
   return(
   <GoogleOAuthProvider clientId={process.env.NEXT_PUBLIC_CLIENT_ID || " "}>
   {!loggedIn && (<GoogleLogin onSuccess={(response)=> {
@@ -23,10 +39,10 @@ export default function Home() {
   onError={()=>{setFailed(true)}}
   />)}
    
-  {loggedIn && ((!failed) ? (
+  {loggedIn && ((!failed) ? (exists ?
     <>
     <Dashboard/>
-    </>
+    </> : <div> Hello </div>
   ): (<p> Error: Not an authorized user. </p>))}
   </GoogleOAuthProvider>
   );
