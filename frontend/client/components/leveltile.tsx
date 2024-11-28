919826c82cb4ee23de7066ddbcebe679c410b8f8
Lucas Tembras <51347268+lucastemb@users.noreply.github.com>
2024-11-28 02:57:51 -0500

    feat: added time formatting for level tile

--- frontend/client/components/leveltile.tsx
+++ frontend/client/components/leveltile.tsx
@@ -9,13 +9,20 @@ interface TaskProps {
 }
 
 export default function LevelTile({task, deleteTask}: TaskProps) {
+    const diffInDays = (timeAchieved: number) => {
+      const currDate: Date = new Date();
+      const taskDate: Date = new Date(timeAchieved * 1000)
 
+      const diffInMs = currDate.getTime() - taskDate.getTime();
+      const daysDiff = Math.floor(diffInMs / (1000 * 60 * 60 *24))
+      return daysDiff
+  }
   return (
    <div className="bg-slate-800/80 text-white rounded-lg mb-4 p-4 w-3/4 max-w-2xl mx-auto drop-shadow-lg">
         <div className="bg-blue-900 text-white rounded-lg p-4 mb-2">
         <div className="flex items-center flex-col space-x-4">
             <p className="text-sm text-gray-300"> <span className="font-bold"> Desired Level:</span> {task.level}</p>
-            <p className="text-gray-300">{task.timeAchieved ? (`Complete ${task.timeAchieved}`) : "Incomplete"}</p>
+            <p className="text-gray-300">{task.timeAchieved ? (`Completed on ${new Date(task.timeAchieved * 1000).toLocaleDateString("en-US")} (${diffInDays(task.timeAchieved)} days ago)`) : "Incomplete"}</p>
         </div>
         </div>
 
