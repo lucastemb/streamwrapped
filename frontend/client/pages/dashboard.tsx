0d8fbade9a9fdc83113a5026b7f6a55fee80c4e6
josecast1 <jc88377@gmail.com>
2024-11-28 17:39:36 -0500

    cleaned up indentation

--- frontend/client/pages/dashboard.tsx
+++ frontend/client/pages/dashboard.tsx
@@ -152,132 +152,133 @@ export default function Dashboard({email, steamId, steamUrl}: DashboardProps) {
   return (
     <>
       {/* Background */}
-      <div className="min-h-screen bg-cover bg-center min-w-[1000px]"
-  style={{
-    backgroundImage: `url('/images/websitebackground.jpg')`,
-    backgroundRepeat: 'no-repeat',
-    backgroundPosition: 'center',
-    backgroundAttachment: 'fixed',
-  }}
->
-  <div className="bg-slate-700/70">
-    <div className="min-h-screen text-white p-2 fade-in-top-to-bottom flex">
-      {/* Left Panel */}
-      <div className="w-auto bg-slate-800/80 p-6 rounded-lg shadow-lg">
-        {/* Profile Section */}
-        {userProfile && (
-          <div className="mb-6">
-            <h2 className="text-lg font-bold mb-4">User Info</h2>
-            <div className="bg-slate-900 flex items-center space-x-4 p-4 rounded-lg">
-              <Image
-                src={`https://avatars.steamstatic.com/${userProfile.avatarhash}_full.jpg`}
-                alt={`${userProfile.personaname} Profile Pic`}
-                width={70}
-                height={70}
-                className="rounded-full"
-              />
-              <p className="text-lg font-semibold">{userProfile.personaname}</p>
-              <button className="bg-slate-600 flex items-center rounded-md px-4 h-12 hover:bg-slate-700 duration-100"
-                onClick={() => window.location.reload()}>
-                Sign Out
-              </button>
-            </div>
-          </div>
-        )}
-
-        {/* Filter Section */}
-        <div className="bg-slate-700/70 p-3 rounded-md flex flex-col space-y-2">
-          <p className="text-lg font-semibold">Filter Tasks:</p>
-          {/* Filter container */}
-          <div className="bg-slate-800 p-2 rounded-md mt-2">
-            <div className="flex flex-col space-y-1">
-              {["No Sorting", "Complete", "Incomplete", "Level-Based", "Achievement-Based", "Friend-Based"].map(
-                (label, index) => (
-                  <button
-                    key={index}
-                    className={`px-4 py-2 rounded-md text-left font-medium duration-150 ${
-                      sortType === index - 1
-                        ? "bg-blue-600 text-white" // Active button
-                        : "text-gray-300 hover:bg-slate-700" // Normal hover
-                    }`}
-                    onClick={() => setSortType(index - 1)}
-                  >
-                    {label}
-                  </button>
-                )
+      <div
+        className="min-h-screen bg-cover bg-center min-w-[1000px]"
+        style={{
+          backgroundImage: `url('/images/websitebackground.jpg')`,
+          backgroundRepeat: 'no-repeat',
+          backgroundPosition: 'center',
+          backgroundAttachment: 'fixed',
+        }}
+      >
+        <div className="bg-slate-700/70">
+          <div className="min-h-screen text-white p-2 fade-in-top-to-bottom flex">
+            {/* Left Panel */}
+            <div className="w-auto bg-slate-800/80 p-6 rounded-lg shadow-lg">
+              {/* Profile Section */}
+              {userProfile && (
+                <div className="mb-6">
+                  <h2 className="text-lg font-bold mb-4">User Info</h2>
+                  <div className="bg-slate-900 flex items-center space-x-4 p-4 rounded-lg">
+                    <Image
+                      src={`https://avatars.steamstatic.com/${userProfile.avatarhash}_full.jpg`}
+                      alt={`${userProfile.personaname} Profile Pic`}
+                      width={70}
+                      height={70}
+                      className="rounded-full"
+                    />
+                    <p className="text-lg font-semibold">{userProfile.personaname}</p>
+                    <button
+                      className="bg-slate-600 flex items-center rounded-md px-4 h-12 hover:bg-slate-700 duration-100"
+                      onClick={() => window.location.reload()}
+                    >
+                      Sign Out
+                    </button>
+                  </div>
+                </div>
               )}
+  
+              {/* Filter Section */}
+              <div className="bg-slate-700/70 p-3 rounded-md flex flex-col space-y-2">
+                <p className="text-lg font-semibold">Filter Tasks:</p>
+                {/* Filter container */}
+                <div className="bg-slate-800 p-2 rounded-md mt-2">
+                  <div className="flex flex-col space-y-1">
+                    {["No Sorting", "Complete", "Incomplete", "Level-Based", "Achievement-Based", "Friend-Based",].map(
+                      (label, index) => (
+                      <button
+                        key={index}
+                        className={`px-4 py-2 rounded-md text-left font-medium duration-150 ${
+                          sortType === index - 1
+                            ? "bg-blue-600 text-white" // Active button
+                            : "text-gray-300 hover:bg-slate-700" // Normal hover
+                        }`}
+                        onClick={() => setSortType(index - 1)}
+                      >
+                        {label}
+                      </button>
+                    ))}
+                  </div>
+                </div>
+              </div>
+            </div>
+  
+            {/* Right Panel */}
+            <div className="w-3/4 ml-2">
+              <div className="flex flex-col items-center mb-4">
+                <Image
+                  src="/images/Steam_icon_logo.png"
+                  alt="Steam Logo"
+                  width={70}
+                  height={70}
+                  className="mb-4 drop-shadow-[0_4px_10px_rgba(0,0,0,0.75)]"
+                />
+                <h1 className="text-3xl font-bold drop-shadow-[0_4px_10px_rgba(0,0,0,0.75)]">
+                  Steam Wrapped
+                </h1>
+              </div>
+  
+              {/* Tasks */}
+              <div>
+                {tasks &&
+                  filterTasks(tasks, sortType).map((task: any) => {
+                    switch (task.type) {
+                      case 1:
+                        return <AchievementTile task={task} deleteTask={deleteTask} />;
+                      case 2:
+                        return <FriendTile task={task} deleteTask={deleteTask} />;
+                      case 3:
+                        return <LevelTile task={task} deleteTask={deleteTask} />;
+                      default:
+                        return null;
+                    }
+                  })}
+              </div>
+  
+              {/* Task Creation */}
+              <div className="flex flex-row justify-around items-center">
+                <Task
+                  steamId={steamId}
+                  steamUrl={steamUrl}
+                  setSubmitted={setSubmitted}
+                  submitted={submitted}
+                />
+                <FriendTask
+                  steamId={steamId}
+                  steamUrl={steamUrl}
+                  setSubmitted={setSubmitted}
+                  submitted={submitted}
+                />
+                <LevelTask
+                  steamId={steamId}
+                  setSubmitted={setSubmitted}
+                  submitted={submitted}
+                />
+              </div>
+  
+              {/* Update Button */}
+              <div className="flex justify-center mt-8">
+                <button
+                  onClick={compareTasks}
+                  className="bg-green-500 text-white rounded px-4 py-2 flex items-center h-12 hover:bg-green-600 duration-100"
+                >
+                  Update
+                </button>
+              </div>
             </div>
           </div>
         </div>
       </div>
-
-      {/* Right Panel */}
-      <div className="w-3/4 ml-2">
-        <div className="flex flex-col items-center mb-4">
-          <Image
-            src="/images/Steam_icon_logo.png"
-            alt="Steam Logo"
-            width={70}
-            height={70}
-            className="mb-4 drop-shadow-[0_4px_10px_rgba(0,0,0,0.75)]"
-          />
-          <h1 className="text-3xl font-bold drop-shadow-[0_4px_10px_rgba(0,0,0,0.75)]">
-            Steam Wrapped
-          </h1>
-        </div>
-
-        {/* Tasks */}
-        <div>
-          {tasks &&
-            filterTasks(tasks, sortType).map((task: any) => {
-              switch (task.type) {
-                case 1:
-                  return <AchievementTile task={task} deleteTask={deleteTask} />;
-                case 2:
-                  return <FriendTile task={task} deleteTask={deleteTask} />;
-                case 3:
-                  return <LevelTile task={task} deleteTask={deleteTask} />;
-                default:
-                  return null;
-              }
-            })}
-        </div>
-
-        {/* Task Creation */}
-        <div className="flex flex-row justify-around items-center">
-          <Task
-            steamId={steamId}
-            steamUrl={steamUrl}
-            setSubmitted={setSubmitted}
-            submitted={submitted}
-          />
-          <FriendTask
-            steamId={steamId}
-            steamUrl={steamUrl}
-            setSubmitted={setSubmitted}
-            submitted={submitted}
-          />
-          <LevelTask
-            steamId={steamId}
-            setSubmitted={setSubmitted}
-            submitted={submitted}
-          />
-        </div>
-
-        {/* Update Button */}
-        <div className="flex justify-center mt-8">
-          <button
-            onClick={compareTasks}
-            className="bg-green-500 text-white rounded px-4 py-2 flex items-center h-12 hover:bg-green-600 duration-100"
-          >
-            Update
-          </button>
-        </div>
-      </div>
-    </div>
-  </div>
-</div>
-
     </>
   );
 }
