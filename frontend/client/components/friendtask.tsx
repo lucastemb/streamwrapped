56a2b3f965e06dfb0a282c9ea5e9689e289fb562
josecast1 <jc88377@gmail.com>
2024-12-02 15:51:52 -0500

    moved the tasks to the top of the page
    -Changed styling, added flex boxes to stand out goal creation more, as well as labeled each goal.

--- frontend/client/components/friendtask.tsx
+++ frontend/client/components/friendtask.tsx
@@ -24,12 +24,17 @@ export default function FriendTask({steamId, steamUrl, setSubmitted, submitted }
   }
 
   return (
-    <div className="flex flex-row items-center">
-      <p className="font-bold"> Desired Friend Count: </p>
-      <input className="text-black m-4" type="number" value={friendCount} onChange={(e)=> setFriendCount(Number(e.target.value))} min="0" step ="1"/>
-    <button className="bg-green-600 text-white rounded px-4 flex items-center h-12" onClick={()=>addTask()}>
-      Submit
-    </button>
+    <div className="flex flex-col justify-center px-2 py-2 m-2 bg-gradient-to-b from-blue-800 to-slate-900 rounded-md">
+      <h1 className="text-lg font-bold text-center mb-2 border-b-2 border-white">Friend Goal</h1>
+      <div className="bg-slate-800 px-2 py-2 rounded-md">
+        <p className="font-semibold"> Desired Friend Count: </p>
+        <input className="text-white mb-1 rounded-sm bg-blue-600 hover:bg-blue-700 duration-100" type="number" value={friendCount} onChange={(e)=> setFriendCount(Number(e.target.value))} min="0" step ="1"/>
+      </div>
+      <div className="flex justify-center items-center">
+        <button className="bg-green-500 text-white rounded px-4 flex items-center h-12 hover:bg-green-600 duration-100 my-1" onClick={()=>addTask()}>
+          Submit
+        </button>
+      </div>
     </div>
   );
 }
--- frontend/client/components/leveltile.tsx
+++ frontend/client/components/leveltile.tsx
@@ -19,16 +19,16 @@ export default function LevelTile({task, deleteTask}: TaskProps) {
   }
   return (
    <div className="bg-slate-800/80 text-white rounded-lg mb-4 p-4 w-3/4 max-w-2xl mx-auto drop-shadow-lg">
-        <div className="bg-blue-900 text-white rounded-lg p-4 mb-2">
+        <div className="bg-blue-800 text-white rounded-lg p-4 mb-2">
         <div className="flex items-center flex-col space-x-4">
-            <p className="text-sm text-gray-300"> <span className="font-bold"> Desired Level:</span> {task.level}</p>
-            <p className="text-gray-300">{task.timeAchieved ? (`Completed on ${new Date(task.timeAchieved * 1000).toLocaleDateString("en-US")} (${diffInDays(task.timeAchieved)} days ago)`) : "Incomplete"}</p>
+            <p className="text-sm text-white"> <span className="font-bold"> Desired Level:</span> {task.level}</p>
+            <p className="text-white">{task.timeAchieved ? (`Completed on ${new Date(task.timeAchieved * 1000).toLocaleDateString("en-US")} (${diffInDays(task.timeAchieved)} days ago)`) : "Incomplete"}</p>
         </div>
         </div>
 
         {/* Buttons Section */}
         <div className="flex justify-between space-x-4">
-        <button className="bg-red-500 text-white rounded px-4 py-2 flex items-center h-12"
+        <button className="bg-red-500 hover:bg-red-600 duration-100 text-white rounded px-4 py-2 flex items-center h-12"
             onClick={() => deleteTask(task._id)}>
             Delete
         </button>
